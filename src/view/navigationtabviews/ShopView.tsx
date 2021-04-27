ec32d58b4877146f4d1cc6541cd858bc959e8a03
Mattias Torstensson <mattiastorstensson91@gmail.com>
2021-04-27 16:57:03 +0200

    pokemon api fix

--- src/view/navigationtabviews/ShopView.tsx
+++ src/view/navigationtabviews/ShopView.tsx
@@ -1,7 +1,33 @@
+import PokemonAPIService from "../../shared/api/service/PokemonAPIService"
+import { useState, useEffect } from "react"
+import { useDebounce } from "../../hooks/useDebounce"
+
 export const ShopView = () => {
+    const [data, setData] = useState<any>()
+    const [search, setSearch] = useState<string>('')
+    const debounceSearchTerm = useDebounce(search, 1000)
+
+    const fetchData = async (search: string) => {
+        if (debounceSearchTerm) {
+            const {data} = await PokemonAPIService.searchCharacter(search)
+            setData(data)
+        }
+    }
+
+    useEffect(() => {
+        fetchData(search)
+    }, [debounceSearchTerm])
+
     return (
         <div>
-            <h1>This is ShopView!</h1>
+            <input placeholder="serach for pokemon"
+                onChange={event => setSearch(event.target.value)}
+            />
+            <h1>name: {data?.name}</h1>
+            <h1>height: {data?.height}</h1>
+            <h1>id: {data?.id}</h1>
+            <h1>weight: {data?.weight}</h1>
+            <h1>type: {data?.types?.[0]?.type?.name}</h1>
         </div>
     )
 } 
